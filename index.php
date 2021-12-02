415d9145d88639c20365e51aa5b130cfada7ee86
Marek_p <marek.mnp@seznam.cz>
2021-12-02 15:53:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php // GalaxyCode Panel v1.0 made by Marek_p 
 
-header("Location: auth/login.php"); // Přesměrování do šložky auth, na soubor login.php pro přihlášení
+header("Location: auth/login.php"); // Přesměrování do šložky auth, na soubor login.php pro přihlášení 
 
 ?>
